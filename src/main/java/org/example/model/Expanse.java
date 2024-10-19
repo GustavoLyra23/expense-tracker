a951a2e4b393c61ed2c12e6f57e833b5473659f4
GustavoLyra23 <gustavolyra23@gmail.com>
2024-10-19 03:45:17 -0300

    added Expanse model

---
+++ src/main/java/org/example/model/Expanse.java
@@ -0,0 +1,67 @@
+package org.example.model;
+
+import java.time.Instant;
+import java.util.Objects;
+
+public class Expanse {
+
+    private String id;
+    private String description;
+    private Double amount;
+    private Instant date;
+
+    public Expanse() {
+    }
+
+    public Expanse(String id, String description, Double amount, Instant date) {
+        this.id = id;
+        this.description = description;
+        this.amount = amount;
+        this.date = date;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public Instant getDate() {
+        return date;
+    }
+
+    public void setDate(Instant date) {
+        this.date = date;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Expanse expanse = (Expanse) o;
+        return Objects.equals(id, expanse.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
