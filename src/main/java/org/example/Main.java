aa625821cc035626a6d0c9c715191db6e5e7af2d
GustavoLyra23 <gustavolyra23@gmail.com>
2024-10-19 03:40:44 -0300

    added addExpense funciton + command validator with regex

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,20 @@
 package org.example;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
+        String regex = "add\\s+--description\\s+\"([^\"]+)\"\\s+--amount\\s+(\\d+(\\.\\d+)?)";
         Scanner sc = new Scanner(System.in);
-        while (true) {
+
+        do {
             var command = sc.nextLine();
             checkCommand(command);
-        }
+            addExpense(command, regex);
+
+        } while (true);
 
 
     }
@@ -18,6 +24,23 @@ public class Main {
             System.exit(0);
         }
     }
+
+    private static void addExpense(String command, String regex) {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(command);
+        if (!matcher.matches()) {
+            System.out.println("Invalid command");
+        } else {
+            System.out.println("added");
+        }
+    }
+
+    private static void createExpanseJson(){
+
+    }
+
+
+
 
 
 }
